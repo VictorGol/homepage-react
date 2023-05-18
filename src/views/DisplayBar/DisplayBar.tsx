6928face2a335fbf0c3b084e281a6e9a36771856
denghaoran <18843011017@qq.com>
2023-05-18 15:33:49 +0800

    optimization

--- src/views/DisplayBar/DisplayBar.tsx
+++ src/views/DisplayBar/DisplayBar.tsx
@@ -1,5 +1,5 @@
 import "./displaybar.css";
-import React, { useMemo } from "react";
+import React from "react";
 import { useAppState } from "../../utils/context/appContext";
 import { highLightStr } from "../../utils/comp";
 import { commands, Command } from "../../utils/constant";
@@ -11,15 +11,12 @@ function DisplayBar(): JSX.Element {
   const x: string = searchValue.toLowerCase();
   const filterArr: Command[] = searchValue ? processCommands(commands, x) : [];
   const list: JSX.Element[] = filterArr.map((v) => (
-    <div
-      className="displaylist"
-      onClick={() => window.open(v.link)}
-      key={v.id}
-      title={v.desc}
-    >
-      {x === "*"
-        ? v.name
-        : highLightStr(v.name, v.name.toLowerCase().indexOf(x), x.length)}
+    <div className="displaylist" key={v.id} title={v.desc}>
+      <a href={v.link} target="_blank">
+        {x === "*"
+          ? v.name
+          : highLightStr(v.name, v.name.toLowerCase().indexOf(x), x.length)}
+      </a>
     </div>
   ));
 

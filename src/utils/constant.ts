21900ad07077f91b05a02d65072547177842ee3f
denghaoran <18843011017@qq.com>
2023-05-22 16:01:20 +0800

    optimization

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -386,6 +386,7 @@ const commands: Command[] = [
     desc: "",
   },
   { name: "qidian", link: "https://www.qidian.com/", tag: ["read"], desc: "" },
+  { name: "qqmail", link: "https://mail.qq.com/", tag: [""], desc: "" },
   { name: "qqmusic", link: "https://y.qq.com/", tag: ["music"], desc: "" },
   {
     name: "radix",
